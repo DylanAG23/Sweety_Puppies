301ff2b68fd49375f7e65abaf14262d2782d7516
DylanAG23 <dylan.avellaneda23@gmail.com>
2025-10-20 16:33:41 -0500

    Inicio para realizar proyecto 2025

--- baseDatos.js
+++ baseDatos.js
@@ -1,10 +1,10 @@
 const { Client } = require('pg');
 
 const client = new Client({
-  host: 'aws-0-us-west-1.pooler.supabase.com',
+  host: 'aws-1-us-east-2.pooler.supabase.com',
   port: 5432,
-  user: 'postgres.onyzutykzjiocjnlaqgr',
-  password: process.env.SUPABASE_PASSWORD || 'sweetypuppies123',
+  user: 'postgres.roqfkukmaayuqffbsggb',
+  password: process.env.SUPABASE_PASSWORD || 'aEwBWJqfJVUBBnto',
   database: 'postgres',
   ssl: { rejectUnauthorized: false }
 });
